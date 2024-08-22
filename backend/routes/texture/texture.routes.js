322252673c2fabecea32aa04cdca073f740dfef0
ArgonautAli <haider.salar@gmail.com>
2024-08-22 19:01:48 +0530

    feat: wrapped middleware

--- backend/routes/texture/texture.routes.js
+++ backend/routes/texture/texture.routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const { createTexture, getTexture, getMostCreated } = require("./texture");
+const { authenticateToken } = require("../../middleware/tokenVerification");
 
 const texture = express.Router();
 
 texture.use(cors());
 texture.use(express.json());
+texture.use(authenticateToken);
 
 texture.use("/create", createTexture);
 texture.use("/get", getTexture);
